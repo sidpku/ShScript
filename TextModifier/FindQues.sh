4a11055b290baa8279d86403c022f1c29cb467b1
Sid <pkusid@gmail.com>
2021-06-16 22:09:58 +0800

    version 1

---
+++ TextModifier/FindQues.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# bash: finding unanswered questions, save in a new file
+# Input: No
+# Constant:
+# `	FILENAME:	file contains my questions
+#	OUTPUT:		FIle to save
+# Author:	Sid(cnqdcyq@163.com)
+# Date:		2021-6-16
+FILENAME='DailyReport.md'
+OUTPUT='UnFinished.md'
+
+
+grep -n '\-\s\[\s\]' $FILENAME > $OUTPUT
